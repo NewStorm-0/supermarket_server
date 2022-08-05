04341c423e3fa295331ea02059503c42842616c7
NewStorm <2456601397@qq.com>
2022-08-05 21:47:40 +0800

    initial

---
+++ src/main/java/com/newstorm/SupermarketServerApplication.java
@@ -0,0 +1,13 @@
+package com.newstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketServerApplication.class, args);
+    }
+
+}
